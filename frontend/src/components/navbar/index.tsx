a46f7c55a153ee651637bd1c2eea0a7e84d76948
unknown <victornerikokado@yahoo.com>
2022-05-13 15:14:35 -0300

    Security Config

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -7,8 +7,8 @@ function Navbar() {
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
-                    <h1>DSMovie </h1>
-                    <a href="https://github.com/victorkok/" target="_blank" rel="noreferrer">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/victorkok" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/victorkok</p>
